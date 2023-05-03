ceb99b9bf3be6cc3fb583319a078a1d5fef94d5e
Liam-Guthrie <130588895+Liam-Guthrie@users.noreply.github.com>
2023-05-03 14:25:41 -0400

    Fixed Things and Added Things
    Fixed: the main method and added code to pick which area you start in

---
+++ theme park/progressBar.java
@@ -0,0 +1,21 @@
+import javax.swing.JProgressBar;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class progressBar extends ThemePark{
+
+//     frame = newJFrame("Attack Bar");
+
+
+//     progresssBar = new JProgressBar(0, 150);
+//     progressBar.setStringPainted(true);
+
+//     button = new JButton("Attack!");
+//     button.addActionListener(event -> attackButton(event));
+
+// }
+
+// public void attackButton(ActionEvent event){
+//     this.score++;
+//     this.progressBar.setValue(this.score);
+}
--- theme park/themepark.java
+++ theme park/themepark.java
@@ -2,6 +2,7 @@ import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+
 import javax.swing.JLabel;
 import javax.swing.JWindow;
 import javax.swing.JTextArea;
@@ -11,7 +12,8 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import javax.swing.JProgressBar;
 
-public class themepark{
+public class ThemePark {
+    // public static void main(String[] args) {
 
     private JFrame frame;
     private JButton button;
@@ -19,59 +21,69 @@ public class themepark{
     private JProgressBar progressBar;
 
     private int buttonPresses = 0;
-    
-    public themepark() {
 
-    frame = new JFrame("Theme Park Game");
-    frame.setSize(500, 500);
+    public static void main(String[] args) {
+        new ThemePark();
+    }
 
-    button = new JButton("Press here to select your character");
-    button.addActionListener(event -> this.CharacterMenu(event));
+    ThemePark() {
+
+        frame = new JFrame("Theme Park Game");
+        frame.setSize(500, 500);
+
+        button = new JButton("Press here to select your character");
+        button.addActionListener(event -> this.characterMenu(event));
 
     }
 
-    public void CharacterMenu(ActionEvent event){
+    public void characterMenu(ActionEvent event) {
         label = new JLabel("You Choose The Area");
         label = new JLabel("Park Employee");
         button = new JButton("Inspect Character");
-        button.addActionListener(e -> this.CharacterInfo(e));
+        button.addActionListener(e -> this.characterInfo(e));
         button = new JButton("Play as Character");
-        button.addActionListener(e -> this.StartGame(event));
+        button.addActionListener(e -> this.startGame(e));
 
     }
 
-    public void CharacterInfo(ActionEvent event){
-    frame = new JFrame("Park Employee");
-    frame.setSize(500, 500);
-    label = new JLabel("Character Bio");
-    label = new JLabel("Character Stats");
-    button = new JButton("Go Back to Menu");
-    button.addActionListener(this.CharacterMenu(event));
-    button = new JButton("Play as Character");
-    button.addActionListener(this.StartGame(event));
+    public void characterInfo(ActionEvent event) {
+        frame = new JFrame("Park Employee");
+        frame.setSize(500, 500);
+        label = new JLabel("Character Bio");
+        label = new JLabel("Character Stats");
+        button = new JButton("Go Back to Menu");
+        button.addActionListener(b -> this.characterMenu(b));
+        button = new JButton("Play as Character");
+        button.addActionListener(b -> this.startGame(b));
     }
 
-    public void StartGame(ActionEvent event){
-        frame = newJFrame("The Game");
+    public void startGame(ActionEvent event) {
+        frame = new JFrame("The Game");
+        label = new JLabel("Which area do you want to start in?");
+        button = new JButton("Area 1");
+        button.addActionListener(a -> this.areaOne(a));
+        button = new JButton("Area 2");
+        button.addActionListener(a -> this.areaTwo(a));
+        button = new JButton("Area 3");
+        button.addActionListener(a -> this.areaThree(a));
+        button = new JButton("Area 4");
+        button.addActionListener(a -> this.areaFour(a));
     }
 
+    public void areaOne(ActionEvent event) {
+        frame = new JFrame("Area 1");
+    }
 
+    public void areaTwo(ActionEvent event) {
+        frame = new JFrame("Area 2");
+    }
 
+    public void areaThree(ActionEvent event) {
+        frame = new JFrame("Area 3");
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void areaFour(ActionEvent event) {
+        frame = new JFrame("Area 4");
+    }
 
 }
