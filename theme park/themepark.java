58f85b7e981bb526f828fa9be0b7de73920ca48a
Liam-Guthrie <130588895+Liam-Guthrie@users.noreply.github.com>
2023-05-05 13:29:10 -0400

    Attractions and Encounters
    added attractions and the possible outcome of meeting one of the bad guys who wants to take over the park

--- theme park/themepark.java
+++ theme park/themepark.java
@@ -12,6 +12,10 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import javax.swing.JProgressBar;
 
+import java.io.FileReader;
+import java.util.Scanner;
+
+
 public class ThemePark {
     // public static void main(String[] args) {
 
@@ -73,7 +77,7 @@ public class ThemePark {
     public void areaOne(ActionEvent event) {
         frame = new JFrame("Area 1");
         label = new JLabel("Which attraction do you want to see?");
-        button = new JButton("Attraction One");
+        button = new JButton("Attraction 1");
         button.addActionListener(m -> this.attractionOneM(m));
         button = new JButton("Attraction 2");
         button.addActionListener(m -> this.attractionTwoM(m));
@@ -83,14 +87,107 @@ public class ThemePark {
 
     public void areaTwo(ActionEvent event) {
         frame = new JFrame("Area 2");
+        label = new JLabel("Which attraction do you want to see?");
+        button = new JButton("Attraction 1");
+        button.addActionListener(h -> this.attractionOneH(h));
+        button = new JButton("Attraction 2");
+        button.addActionListener(h -> this.attractionTwoH(h));
+        button = new JButton("Attraction 3");
+        button.addActionListener(h -> this.attractionThreeH(h));
     }
 
     public void areaThree(ActionEvent event) {
         frame = new JFrame("Area 3");
+        label = new JLabel("Which attraction do you want to see?");
+        button = new JButton("Attraction 1");
+        button.addActionListener(e -> this.attractionOneE(e));
+        button = new JButton("Attraction 2");
+        button.addActionListener(e -> this.attractionTwoE(e));
+        button = new JButton("Attraction 3");
+        button.addActionListener(e -> this.attractionThreeE(e));
     }
 
     public void areaFour(ActionEvent event) {
         frame = new JFrame("Area 4");
+        label = new JLabel("Which attraction do you want to see?");
+        button = new JButton("Attraction 1");
+        button.addActionListener(a -> this.attractionOneA(a));
+        button = new JButton("Attraction 2");
+        button.addActionListener(a -> this.attractionTwoA(a));
+        button = new JButton("Attraction 3");
+        button.addActionListener(a -> this.attractionThreeA(a));
     }
+
+    public void attractionOneM(ActionEvent event) {
+        frame = new JFrame("Attraction One");
+        boolean randomEncounter = true;
+        while (randomEncounter == true){
+            try (Scanner in = new Scanner("randomEncounter.txt")) {
+                String output = in.next();
+                label = new JLabel(output);
+        }
+        label = new JLabel("What do you do?");
+        button = new JButton("1- Run away");
+        button = new JButton("2- Engage in an anime battle");
+        button = new JButton("3- Make peace"); 
+    }
+
+    }
+
+    public void attractionTwoM(ActionEvent event) {
+        frame = new JFrame("Attraction Two");
+
+    }
+
+    public void attractionThreeM(ActionEvent event) {
+        frame = new JFrame("Attraction Three");
+
+    }
+
+    public void attractionOneH(ActionEvent event) {
+        frame = new JFrame("Attraction One");
+
+    }
+
+    public void attractionTwoH(ActionEvent event) {
+        frame = new JFrame("Attraction Two");
+
+    }
+
+    public void attractionThreeH(ActionEvent event) {
+        frame = new JFrame("Attraction Three");
+
+    }
+
+    public void attractionOneE(ActionEvent event) {
+        frame = new JFrame("Attraction One");
+
+    }
+
+    public void attractionTwoE(ActionEvent event) {
+        frame = new JFrame("Attraction Two");
+
+    }
+
+    public void attractionThreeE(ActionEvent event) {
+        frame = new JFrame("Attraction Three");
+
+    }
+
+    public void attractionOneA(ActionEvent event) {
+        frame = new JFrame("Attraction One");
+
+    }
+
+    public void attractionTwoA(ActionEvent event) {
+        frame = new JFrame("Attraction Two");
+
+    }
+
+    public void attractionThreeA(ActionEvent event) {
+        frame = new JFrame("Attraction Three");
+
+    }
+
 
 }
